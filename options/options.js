ee9afdf752a63fcb177d84db22025206e3f045cc
Crowdbotics <team@crowdbotics.com>
2023-08-31 13:51:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-hill-43165",
+  url: "https://solitary-hill-43165.botics.co",
+  api: "https://solitary-hill-43165.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
